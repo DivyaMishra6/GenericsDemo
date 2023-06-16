5107fbbed8eaf93329dddc93b016108a07f38306
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-16 19:36:20 +0530

    [Divya]:  Using Generic find Max Size

--- FindMax.cs
+++ FindMax.cs
@@ -6,108 +6,146 @@ using System.Threading.Tasks;
 
 namespace GenericsDemo
 { 
-    public class FindMax
+    public class FindMax<T>
     {
-        //integer
-        public static int Maximum(int num1, int num2, int num3)
+        public T value1, value2, value3;
+        public FindMax(T value1, T value2, T value3)
         {
-          if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
-
-                num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
-
-               num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
-          {
-               return num1;
-          }
-          if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
-
-             num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
-
-             num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
-          {
-                return num2;
-          }
-
-          if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
-
-              num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
-
-              num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
-          {
-               return num3;
-          }
-
-           return default(int);
-
+            this.value1 = value1;
+            this.value2 = value2;
+            this.value3 = value3;
         }
 
-        //float
-        public static float Maximumfloat(float num1, float num2, float num3)
+        public T MaximumUsingGenerics<T>(T value1, T value2, T value3) where T : IComparable
         {
-            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0 ||
 
-                num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+                value1.CompareTo(value2) >= 0 && value1.CompareTo(value3) > 0 ||
 
-                 num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+                value1.CompareTo(value2) > 0 && value1.CompareTo(value3) >= 0)
             {
-                return num1;
+                return value1;
             }
-            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+            if (value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0 ||
 
-               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+                 value2.CompareTo(value1) >= 0 && value2.CompareTo(value3) > 0 ||
 
-               num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+                 value2.CompareTo(value1) > 0 && value2.CompareTo(value3) >= 0)
             {
-                return num2;
-            }
-
-            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
-
-               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
-
-               num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
-            {
-                return num3;
+                return value2;
             }
 
-            return default(float);
+            if (value3.CompareTo(value1) > 0 && value3.CompareTo(value2) > 0 ||
 
-        }
+                 value3.CompareTo(value1) >= 0 && value3.CompareTo(value2) > 0 ||
 
-        //String
-
-        public static String MaximumString(String name1, String name2, String name3)
-        {
-            if (name1.CompareTo(name2) > 0 && name1.CompareTo(name3) > 0 ||
-
-                name1.CompareTo(name2) >= 0 && name1.CompareTo(name3) > 0 ||
-
-                name1.CompareTo(name2) > 0 && name1.CompareTo(name3) >= 0)
+                 value3.CompareTo(value1) > 0 && value3.CompareTo(value2) >= 0)
             {
-                return name1;
-            }
-            if (name2.CompareTo(name1) > 0 && name2.CompareTo(name3) > 0 ||
-
-                name2.CompareTo(name1) >= 0 && name2.CompareTo(name3) > 0 ||
-
-               name2.CompareTo(name1) > 0 && name2.CompareTo(name3) >= 0)
-            {
-                return name2;
-            }
-
-            if (name3.CompareTo(name1) > 0 && name3.CompareTo(name2) > 0 ||
-
-               name3.CompareTo(name1) >= 0 && name3.CompareTo(name2) > 0 ||
-
-                name3.CompareTo(name1) > 0 && name3.CompareTo(name2) >= 0)
-            {
-                return name3;
+                return value3;
             }
 
             return default;
-
-
         }
+        //integer
+        //public static int Maximum(int num1, int num2, int num3)
+        //{
+        //  if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+
+        //        num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+
+        //       num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+        //  {
+        //       return num1;
+        //  }
+        //  if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+
+        //     num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+
+        //     num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+        //  {
+        //        return num2;
+        //  }
+
+        //  if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+
+        //      num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+
+        //      num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+        //  {
+        //       return num3;
+        //  }
+
+        //   return default(int);
+
+        //}
+
+        ////float
+        //public static float Maximumfloat(float num1, float num2, float num3)
+        //{
+        //    if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+
+        //        num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+
+        //         num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+        //    {
+        //        return num1;
+        //    }
+        //    if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+
+        //       num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+
+        //       num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+        //    {
+        //        return num2;
+        //    }
+
+        //    if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+
+        //       num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+
+        //       num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+        //    {
+        //        return num3;
+        //    }
+
+        //    return default(float);
+
+        //}
+
+        ////String
+
+        //public static String MaximumString(String name1, String name2, String name3)
+        //{
+        //    if (name1.CompareTo(name2) > 0 && name1.CompareTo(name3) > 0 ||
+
+        //        name1.CompareTo(name2) >= 0 && name1.CompareTo(name3) > 0 ||
+
+        //        name1.CompareTo(name2) > 0 && name1.CompareTo(name3) >= 0)
+        //    {
+        //        return name1;
+        //    }
+        //    if (name2.CompareTo(name1) > 0 && name2.CompareTo(name3) > 0 ||
+
+        //        name2.CompareTo(name1) >= 0 && name2.CompareTo(name3) > 0 ||
+
+        //       name2.CompareTo(name1) > 0 && name2.CompareTo(name3) >= 0)
+        //    {
+        //        return name2;
+        //    }
+
+        //    if (name3.CompareTo(name1) > 0 && name3.CompareTo(name2) > 0 ||
+
+        //       name3.CompareTo(name1) >= 0 && name3.CompareTo(name2) > 0 ||
+
+        //        name3.CompareTo(name1) > 0 && name3.CompareTo(name2) >= 0)
+        //    {
+        //        return name3;
+        //    }
+
+        //    return default;
+
+
+        //}
 
     }
 }
