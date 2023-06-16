ab826447c506680e22babefc957b420cb5f543a1
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-16 13:04:05 +0530

    Welcome to GenericsDemo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericsDemo
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            int result = FindMax.Maximum(55, 88, 33);
+            Console.WriteLine("maximum number is: " + result);
+
+            
+
+        }
+    }
+}
