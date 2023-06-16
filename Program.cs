dd8ce12413d0f8ab653d1dead8a4a8ebd3403db1
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-16 18:40:15 +0530

    [Divya]: Enter Three float number and find Max

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@
             int result = FindMax.Maximum(55, 88, 33);
             Console.WriteLine("maximum number is: " + result);
 
-            
+            float result1 = FindMax.Maximumfloat(5.7f, 8.08f, 33.67f);
+            Console.WriteLine("maximum float is: " + result1);
+
 
         }
     }
