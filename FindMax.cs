6f506d17b848c74c4addde5337b7687190c33c90
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-16 18:54:22 +0530

    [Divya]: Enter Three float number and find Max

--- FindMax.cs
+++ FindMax.cs
@@ -8,7 +8,7 @@ namespace GenericsDemo
 { 
     public class FindMax
     {
-        
+        //integer
         public static int Maximum(int num1, int num2, int num3)
         {
           if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
@@ -39,10 +39,40 @@ namespace GenericsDemo
 
            return default(int);
 
+        }
+
+        //float
+        public static float Maximumfloat(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+
+                num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+
+                 num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+
+            return default(float);
 
         }
 
-        
-        
     }
 }
